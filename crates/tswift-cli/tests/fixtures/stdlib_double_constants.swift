e34554cbeaa522605e422ad8bc2baa260db3147c
Francis Chong <francis@goodnotes.com>
2026-06-28 19:39:01 +0100

    feat(core): add Double floating-point type constants

---
+++ crates/tswift-cli/tests/fixtures/stdlib_double_constants.swift
@@ -0,0 +1,8 @@
+// Double floating-point type constants.
+print(Double.pi)
+print(Double.infinity)
+print(Double.nan.isNaN)
+print(Double.greatestFiniteMagnitude > 1e300)
+print(Double.leastNonzeroMagnitude > 0.0)
+print(Double.leastNormalMagnitude > 0.0)
+print(Double.ulpOfOne > 0.0)
